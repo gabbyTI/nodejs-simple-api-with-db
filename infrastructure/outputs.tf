0fe45659d15a0deeaf508bb13bc8ada4bd526809
Gabriel Ibenye <gabrielibenye@gmail.com>
2025-07-12 11:30:19 -0400

    feat(outputs): add output for application server public IP

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -8,6 +8,11 @@ output "vpc_cidr" {
   value       = module.vpc.vpc_cidr
 }
 
+output "app_server_ip" {
+  description = "Public IP of the application server"
+  value       = module.app_server.public_ip
+}
+
 output "db_endpoint" {
   description = "Database endpoint"
   value       = module.postgres_db.endpoint
